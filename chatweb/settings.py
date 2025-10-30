12297696c83792a1f03df02f0750fd7c96e87d7a
Urvish Bhatt <urvbht710@gmail.com>
2025-10-31 00:22:55 +0530

    env added.

--- chatweb/settings.py
+++ chatweb/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,8 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+env_file = Path.joinpath(BASE_DIR, '.env.local')
+if Path.is_file(env_file):
+    load_dotenv(env_file)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-as=+m4djyag1-$wo_5*e9^8^6^&%#-cag(l^i8j^+&0vao(*un'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
